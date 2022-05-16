3d67d5911db34120cd23e221067758abc05300f6
llvovll89 <svvvs5579@naver.com>
2022-05-16 18:07:39 +0900

    three

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
-const titleClick = document.querySelector('.title');
-const hidetemp = document.querySelector('.hide-temp');
+const titleClick = document.querySelector(".title");
+const hidetemp = document.querySelector(".hide-temp");
+const typingText = document.querySelector(".typing-text p"),
+  inpField = document.querySelector(".wrapper .input-field");
 
-titleClick.addEventListener('click', (e)=> {
-    e.preventDefault();
-    hidetemp.style.display = "none";
-})
+let charIndex = 0;
+
+// start background
+titleClick.addEventListener("click", (e) => {
+  e.preventDefault();
+  hidetemp.style.display = "none";
+});
+
+function randomParagraph() {
+  let randIndex = Math.floor(Math.random() * paragraph.length);
+  paragraph[randIndex].split("").forEach((span) => {
+    let spanTag = `<span>${span}</span>`;
+    typingText.innerHTML += spanTag;
+  });
+  document.addEventListener("keydown", () => inpField.focus());
+  typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+  const characters = typingText.querySelectorAll("span");
+  let typedChar = inpField.value.split("")[charIndex];
+  if (characters[charIndex].innerText === typedChar) {
+    characters[charIndex].classList.add("correct");
+  } else {
+    characters[charIndex].classList.add("incorrect");
+  }
+  charIndex++;
+  characters[charIndex].classList.add('active');
+}
+
+randomParagraph();
+inpField.addEventListener("input", initTyping);
